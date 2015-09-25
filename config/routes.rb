c18894885d6a871de758928d3f4ad28316f769b3
askl56 <amscott@openmailbox.org>
2015-09-25 10:33:08 +0100

    added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'blog/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
